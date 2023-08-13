441a923e10c806c5bfb8e586536c71098a1c7b10
Nick Nithee <nnuankaew@seekasia.com>
2023-08-13 22:06:23 +0700

    Init project

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <main>
+      <div className="head-title">onboarding</div>
+    </main>
+  );
+};
+
+export default page;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,40 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import TopBar from "@/components/shared/TopBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
+import BottomBar from "@/components/shared/BottomBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = () => {
+  return {
+    title: "Threads",
+    description: "This is a demo application for Nest.js",
+  };
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar />
+          <main>
+            <LeftSideBar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSideBar />
+          </main>
+          <BottomBar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
